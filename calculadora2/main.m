39f30e85969a69bea8abe2b693f527f202b96030
Jazmin Hernandez <jhernandez@redrabbit.mx>
2014-08-15 10:23:22 -0600

    Initial Commit

---
+++ calculadora2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  calculadora2
+//
+//  Created by Jazmin Hernandez on 15/08/14.
+//  Copyright (c) 2014 redrabbit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
